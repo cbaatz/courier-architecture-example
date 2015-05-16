1db7c0750031b3cfda4530fd1c8331924715754d
Carl Baatz <carl.baatz@gmail.com>
2015-05-16 19:47:20 +0200

    Extend example with hash routing and comments.

--- README.md
+++ README.md
@@ -2,18 +2,24 @@ Minimal Courier architecture example
 ====================================
 
 The repository demonstrates a minimal example of a client architecture
-described briefly in
-http://baatz.io/posts/courier-reactive-react-architecture/, using reactive
-programming (dataflow) and  immediate mode UI rendering:
+described at http://baatz.io/posts/courier-reactive-react-architecture/, using
+reactive programming (dataflow) and immediate mode UI rendering:
 
 ![A diagram of the courier architecture](courier.png)
 
+The code is heavily commented and intended to be easy to skim. In a realistic
+application, the different parts would be split into their own modules / files,
+but I've kept them in one file to make it easier to get an overview.
+
 This example uses Kefir and React, but you can use whichever libraries you
-prefer.
+prefer. To try the example, do:
 
     npm install .
     npm start
 
-If you want to experiment, you do `node_modules/gulp/bin/gulp.js watch` to run
-a watcher. You can also get live reload with the LiveReload [Chrome
+If you want to experiment, you run a watcher with:
+
+    node_modules/gulp/bin/gulp.js watch
+
+You can also get live reload with the LiveReload [Chrome
 extension](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei).
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,36 +1,83 @@
 var Kefir = require("kefir");
 var React = require("react");
 
-courier = {};
-courier.add = Kefir.pool();
-courier.reset = Kefir.pool();
-courier.messages = Kefir.merge(
-    [ courier.add.map(function() { return "add"; })
-    , courier.reset.debounce(750).map(function() { return "reset"; })
-    ]);
+/* COURIER */
 
-var initState = { count: 0 };
+/* Courier services are defined as Kefir pools so that we can plug in streams
+ * and properties dynamically from the UI and the transition function. The
+ * courier composes a network of streams which results in a single messages
+ * stream to be used together with the transition function to generate the
+ * state property. */
+
+var services = {};
+services.hash = Kefir.pool();
+services.add = Kefir.pool();
+services.reset = Kefir.pool();
+
+messages = Kefir.merge(
+    [ services.add.map(function() { return { type: "add" }; })
+    , services.reset.debounce(1000).map(function() { return { type: "reset" }; })
+    , services.hash.map(function(hash) { return { type: "hash", hash: hash }; })
+    ]).log("MESSAGE");
+
+/* STATE TRANSITION */
+
+/* The transition function defines how the application progresses. It takes the
+ * previous state, initially initState, and a message and produces the state
+ * property. */
+
+var initState = { count: 0, hash: "0" };
 function transition(state, message) {
-    switch (message) {
+
+    switch (message.type) {
         case "add":
-            courier.reset.plug(Kefir.constant(true));
-            return { count: state.count + 1 }; break;
+            services.reset.plug(Kefir.constant(true));
+            return { count: state.count + 1, hash: Number(state.count + 1).toString() }; break;
         case "reset":
             return initState; break;
+        case "hash":
+
+            /* In a realistic application you would use a routing library to
+             * parse the hash (or URL if using History API) and perform the
+             * appropriate state transition based on that. */
+
+            var count = parseInt(message.hash);
+            if (count !== 0) services.reset.plug(Kefir.constant(true));
+            if (isNaN(count)) return state; // Reject hash change
+            return { count: count, hash: message.hash };
+            break;
         default:
             return state;
     }
 };
 
+/* UI */
+
+/* The user interface is a React component which gets the courier services and
+ * the states property as props. It subscribes to the states property so it can
+ * update its internal state whenever the application state changes. It sends
+ * interactions to the courier services based on user interaction events. */
+
 var UI = React.createClass({
     componentWillMount: function() {
-        /* Since states is a Kefir property, onValue will fire
-           synchronously with the initial state which means we
-           don't need getInitialState. */
+
+        /* Since states is a Kefir property, onValue will fire synchronously
+         * with the initial state which means we don't need getInitialState. */
+
         this.props.states.onValue(this.setState.bind(this));
+
+        /* There is nothing stopping subcomponents from managing their own
+         * state, but only if that state is truly local to that component. If
+         * the state is used by parent or sibling components, it should be
+         * handled in the state transition function. */
     },
     add: function(event) {
-        this.props.courier.add.plug(Kefir.constant(true));
+
+        /* When the UI captures the click event it sends an instruction to the
+         * courier's add service, which will result in a message, a state
+         * transition, and a .setState call. */
+
+        this.props.services.add.plug(Kefir.constant(true));
     },
     render: function() {
         return (
@@ -42,6 +89,79 @@ var UI = React.createClass({
     }
 });
 
-var states = courier.messages.scan(transition, initState);
-React.render(<UI courier={courier} states={states} />,
+/* URL */
+
+function initHash(hashService, hashStates) {
+
+    /* NOTE: This function would not be written from scratch in each
+     * application, but provided by a library as it is not domain specific. */
+     
+    /* This function sets up basic URL hash handling, allowing us to treat hash
+     * changes just like other UI changes in the transition function and the
+     * courier. A more advanced version could use the History API to implement
+     * similar functionality.
+     *
+     * We make sure the state's hash is reflected in the browser and we send
+     * instructions to the courier's hash service for hash changes triggered by
+     * the user or browser so the transition function can handle them.
+     *
+     * You should think of the browser address bar as a special UI component
+     * that renders itself based on the state's hash property and produces
+     * instructions for the courier's hash service when the user changes it. */
+
+    /* muteNext mutes the next hashchange event and is used to avoid an
+     * instruction for state transition induced hash changes. */
+
+    var muteNext = false;
+    function mute() {
+        if (muteNext) {
+            muteNext = false;
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    function getHash() { return window.location.hash.replace(/^#/, ""); }
+    function setHash(hash) {
+        muteNext = (hash !== getHash());
+        window.location.hash = hash;
+    }
+
+    hashService.plug(Kefir.fromEvents(window, "hashchange", getHash).filter(mute));
+
+    /* To keep the initState and transition function unifrom we send the
+     * initial browser hash as a normal instruction. To achieve this, we first
+     * handle the initialHash. If it's empty we set the hash to the hash
+     * specified in the initial state, otherwise we send the hash to the
+     * courier's hash service so it can be handled by the transition function.
+     * 
+     * Because of how Kefir manages activation/deactivation, we set up the skip
+     * subscriber first so that hashService stays activated (we assume
+     * hashStates is a function of hashService). */
+
+    hashStates.skip(1).onValue(setHash);
+    hashStates.take(1).onValue(function (initHash) {
+        var hash = getHash();
+        if (hash === "") { setHash(initHash); }
+        else { hashService.plug(Kefir.constant(hash)); }
+    });
+};
+
+/* INITIALIZATION */
+
+/* Fold (scan in Kefir parlance) messages with the transition function to
+ * produce a states property. */
+
+var states = messages.scan(transition, initState);
+
+/* Initialize hash handling by passing the courier's hash service and a state
+ * hash property. */
+
+initHash(services.hash, states.map(function(s) { return s.hash; }));
+
+/* Initialize the UI by using React's render function, passing the courier's
+ * services and the states property as props. */
+
+React.render(<UI services={services} states={states} />,
              document.getElementById('app'));
